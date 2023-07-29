f2a453b0c7a2b5510035c1e686f49dd34f8cbe7d
于骏浩 <shuimuqinghua@yujunhaodeMacBook-Pro.local>
2023-07-29 21:20:26 +0800

    feat: add agnet5 and agent6

--- src/agent.py
+++ src/agent.py
@@ -140,7 +140,7 @@ class GraphAgent4(Graph):
                 agent_event - 1,
                 self.node_list[self.name_id_dict[self.agent_history[agent_event]]].name,
             )
-            return numerator / (denominator * 39)
+            return numerator / (denominator)
 
     @lru_cache(maxsize=1000)
     def prediction(self, agent_event, target_name):
@@ -178,5 +178,161 @@ class GraphAgent4(Graph):
         #         max_belief_num += 1
         # print(max_belief_num)
         self.agent_name = max(self.node_list, key=lambda x: x.belief).name
+        self.agent_history.append(self.agent_name)
+        self.node_list[self.name_id_dict[self.agent_name]].status = 1
+
+class GraphAgent5(Graph):
+    def __init__(self):
+        super().__init__()
+    
+    def initialize(self, random_seed=0):
+        super().initialize(random_seed)
+        # initialize the belief of each node to be equal and sum to 1
+        for node in self.node_list:
+            node.belief = 1 / self.node_num
+        self.agent_history = []  # record the agent's location history
+        self.agent_history.append(self.agent_name)
+    
+
+    @lru_cache(maxsize=1000)
+    def filter(self, agent_event, target_name):
+        if agent_event == 0:
+            return 1 / self.node_num
+        else:
+            if self.agent_history[agent_event] == target_name:
+                return 0
+            numerator = 0
+            for neighbor in self.node_list[
+                self.name_id_dict[target_name]
+            ].neighbor_list:
+                # print((
+                #     self.filter(agent_event - 1, neighbor)
+                # ))
+                numerator += 1 / (self.node_list[self.name_id_dict[neighbor]].degree) * (
+                    self.filter(agent_event - 1, neighbor)
+                )
+
+            denominator = self.prediction(
+                agent_event - 1,
+                self.node_list[self.name_id_dict[self.agent_history[agent_event]]].name,
+            )
+            return numerator / (denominator)
+
+    @lru_cache(maxsize=1000)
+    def prediction(self, agent_event, target_name):
+        if agent_event == 0:
+            return 1 / self.node_num
+        belief = 0
+        for neighbor in self.node_list[self.name_id_dict[target_name]].neighbor_list:
+            belief += (
+                self.filter(agent_event, neighbor)
+                * 1
+                / self.node_list[self.name_id_dict[neighbor]].degree
+            )
+
+        return belief
+
+    def plot(self, step, save_dir="../results/figs/", plot_belief=True):
+        super().plot(step, save_dir, plot_belief)
+
+    def move_agent(self):
+        self.node_list[self.name_id_dict[self.agent_name]].status = 0
+        for node in self.node_list:
+            node.belief = self.prediction(len(self.agent_history) - 1, node.name)
+
+        # if there is multiple nodes with the same belief, choose one whose neighbor has the highest belief
+        max_belief = max(self.node_list, key=lambda x: x.belief).belief
+        max_belief_nodes = []
+        for node in self.node_list:
+            if node.belief == max_belief:
+                max_belief_nodes.append(node)
+        max_neighbor_belief = 0
+        for node in max_belief_nodes:
+            for neighbor in node.neighbor_list:
+                if self.node_list[self.name_id_dict[neighbor]].belief > max_neighbor_belief:
+                    max_neighbor_belief = self.node_list[self.name_id_dict[neighbor]].belief
+                    self.agent_name = node.name
+        self.agent_history.append(self.agent_name)
+        self.node_list[self.name_id_dict[self.agent_name]].status = 1
+
+class GraphAgent6(Graph):
+    def __init__(self):
+        super().__init__()
+    
+    def initialize(self, random_seed=0):
+        super().initialize(random_seed)
+        # initialize the belief of each node to be equal and sum to 1
+        for node in self.node_list:
+            node.belief = 1 / self.node_num
+        self.agent_history = []  # record the agent's location history
+        self.agent_history.append(self.agent_name)
+    
+
+    @lru_cache(maxsize=1000)
+    def filter(self, agent_event, target_name):
+        if agent_event == 0:
+            return 1 / self.node_num
+        else:
+            if self.agent_history[agent_event] == target_name:
+                return 0
+            numerator = 0
+            for neighbor in self.node_list[
+                self.name_id_dict[target_name]
+            ].neighbor_list:
+                # print((
+                #     self.filter(agent_event - 1, neighbor)
+                # ))
+                numerator += 1 / (self.node_list[self.name_id_dict[neighbor]].degree) * (
+                    self.filter(agent_event - 1, neighbor)
+                )
+
+            denominator = self.prediction(
+                agent_event - 1,
+                self.node_list[self.name_id_dict[self.agent_history[agent_event]]].name,
+            )
+            return numerator / (denominator)
+
+    @lru_cache(maxsize=1000)
+    def prediction(self, agent_event, target_name):
+        if agent_event == 0:
+            return 1 / self.node_num
+        belief = 0
+        for neighbor in self.node_list[self.name_id_dict[target_name]].neighbor_list:
+            belief += (
+                self.filter(agent_event, neighbor)
+                * 1
+                / self.node_list[self.name_id_dict[neighbor]].degree
+            )
+
+        return belief
+    
+    def plot(self, step, save_dir="../results/figs/", plot_belief=True):
+        super().plot(step, save_dir, plot_belief)
+
+    def _find_shortest_path(self, target_name):
+        # use BFS to get the shortest path from current location to target, then return the next location
+        visited = [False] * self.node_num
+        queue = []
+        queue.append((self.agent_name, []))  # (node name, path using node name)
+        visited[self.name_id_dict[self.agent_name]] = True
+
+        while queue:
+            s, path = queue.pop(0)
+            path.append(s)
+            if s == target_name:
+                return path[1] if len(path) > 1 else path[0]
+            for i in self.node_list[self.name_id_dict[s]].neighbor_list:
+                if not visited[self.name_id_dict[i]]:
+                    queue.append((i, path + [i]))
+                    visited[self.name_id_dict[i]] = True
+        return -1
+    
+    def move_agent(self):
+        self.node_list[self.name_id_dict[self.agent_name]].status = 0
+        for node in self.node_list:
+            node.belief = self.prediction(len(self.agent_history) - 1, node.name)
+
+        next_location = self._find_shortest_path(max(self.node_list, key=lambda x: x.belief).name)
+        self.agent_name = next_location
         self.agent_history.append(self.agent_name)
         self.node_list[self.name_id_dict[self.agent_name]].status = 1
--- src/simulation.sh
+++ src/simulation.sh
@@ -1,5 +1,5 @@
 python3 analysis.py \
---num_simulations 20 \
---agents 4 \
+--num_simulations 10 \
+--agents 6 \
 --verbose \
 --visualize
