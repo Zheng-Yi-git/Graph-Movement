8060ab9f63cba569bf697d04763d6a348d04c641
于骏浩 <shuimuqinghua@yujunhaodeMacBook-Pro.local>
2023-07-29 22:36:28 +0800

    feat: add agent7

--- src/agent.py
+++ src/agent.py
@@ -112,7 +112,6 @@ class GraphAgent2(Graph):
         self.agent_name = self._get_path()
         self.node_list[self.name_id_dict[self.agent_name]].status = 1
 
-
 class GraphAgent4(Graph):
     def __init__(self):
         super().__init__()
@@ -399,12 +398,75 @@ class GraphAgent7(Graph):
     def plot(self, step, save_dir="../results/figs/", plot_belief=True):
         super().plot(step, save_dir, plot_belief)
 
-    def _find_shortest_path(self, target_name):
-        # use BFS to get the shortest path from current location to target, then return the next location
-        visited = [False] * self.node_num
+    def _BFS(self, agent_name, target_name):
+        """
+        return the length of the shortest path from curr_location to target_location and the number of shortest paths
+        """
+        visited = [False] * 40
         queue = []
-        queue.append((self.agent_name, []))  # (node name, path using node name)
-        visited[self.name_id_dict[self.agent_name]] = True
+        queue.append((agent_name, [self.agent_name]))
+        visited[self.name_id_dict[agent_name]] = True
+        shortest_paths = []
 
         while queue:
-            s, path = queue
+            s, path = queue.pop(0)
+            # path.append(s)
+            if s == target_name and (
+                len(shortest_paths) == 0 or len(path) == len(shortest_paths[0])
+            ):
+                shortest_paths.append(path)
+            for i in self.node_list[self.name_id_dict[s]].neighbor_list:
+                if not visited[self.name_id_dict[i]]:
+                    queue.append((i, path + [i]))
+                    visited[self.name_id_dict[i]] = True
+        if len(shortest_paths) == 0:
+            return 0, 0, None
+        elif len(shortest_paths) == 1:
+            return len(shortest_paths[0]) - 1, 1, shortest_paths[0]
+        else:
+            return len(shortest_paths[0]) - 1, len(shortest_paths), None
+
+    def _get_path(self, target_name):
+        """
+        find the shortest expected distance from agent's next location to target's next location, return the next location
+        """
+        # first we should check if there exists more than one shortest path from agent to target
+        shortest_path_length, num_shortest_paths, shortest_path = self._BFS(
+            self.agent_name, target_name
+        )
+        if num_shortest_paths == 1:
+            return shortest_path[1] if shortest_path_length > 0 else shortest_path[0]
+        expected_distances: dict = {}
+
+        for next_location in self.node_list[
+            self.name_id_dict[self.agent_name]
+        ].neighbor_list + [self.agent_name]:
+            expected_distances[next_location] = 0  # expected_distance
+            total_num_shortest_paths = 0
+            total_shortest_path_length = 0
+
+            for target_location in self.node_list[
+                self.name_id_dict[target_name]
+            ].neighbor_list:
+                shortest_path_length, num_shortest_paths = self._BFS(
+                    next_location, target_location
+                )
+                total_shortest_path_length += shortest_path_length * num_shortest_paths
+                total_num_shortest_paths += num_shortest_paths
+
+            expected_distances[next_location] = (
+                total_shortest_path_length / total_num_shortest_paths
+            )
+
+        # find the next location with smallest expected distance
+        return min(expected_distances, key=expected_distances.get)
+    
+    def move_agent(self):
+        self.node_list[self.name_id_dict[self.agent_name]].status = 0
+        for node in self.node_list:
+            node.belief = self.prediction(len(self.agent_history) - 1, node.name)
+
+        next_location = self._get_path(max(self.node_list, key=lambda x: x.belief).name)
+        self.agent_name = next_location
+        self.agent_history.append(self.agent_name)
+        self.node_list[self.name_id_dict[self.agent_name]].status = 1
--- src/simulation.sh
+++ src/simulation.sh
@@ -1,5 +1,5 @@
 python3 analysis.py \
 --num_simulations 10 \
---agents 6 \
+--agents 7 \
 --verbose \
 --visualize
